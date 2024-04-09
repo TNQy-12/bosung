e8d9d95eeb077bbb7f2278d32bf0884e08ca84b0
TNQy-12 <165918821+TNQy-12@users.noreply.github.com>
2024-04-10 00:49:22 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <form action="register2.php" method="post">
+        <label for="mail">mail</label>
+        <input type="mail" name="mail"><br>
+        <label for="pass">password</label>
+        <input type="pass" name="pass">
+        <br>
+        <input type="submit" style="background-color: rgb(166, 166, 166);" value="Submit">
+
+    </form>
+
+</body>
+
+</html>
